b9d7df147689975aee6dceb56a266f09895e7873
dawun <dawun@lacy.co.kr>
2023-11-28 15:15:06 +0900

    그래프 사진이 변경 안되는 이슈 수정

--- static/js/graph.js
+++ static/js/graph.js
@@ -14,9 +14,24 @@ $(document).ready(function () {
             dataType: 'json',
             success: function (result) {
                 if (result.result == 'true') {
-                    $('#graphContainer').show();
+                    genGraphUI();
                 }
             }
         });
     });
+    
+    function genGraphUI () {
+        $('#graphContainer').html('');
+        $('#graphContainer').append(
+            '            <div class="row row-cols-1 row-cols-sm-2 g-4">' +
+            '                <div class="col d-flex flex-column gap-2">' +
+            '                    <h4 class="fw-semibold mb-0 text-body-emphasis">언어별 요청 수</h4>' +
+            '                  <img class="bi" src="static/img/language_analysis.png?time=' + Math.floor((new Date()).getTime() / 1000) + '">' +
+            '                </div>' +
+            '                <div class="col d-flex flex-column gap-2">' +
+            '                    <h4 class="fw-semibold mb-0 text-body-emphasis">소스코드 검증 성공/실패</h4>' +
+            '                    <img class="bi" src="static/img/result_analysis.png?time=' + Math.floor((new Date()).getTime() / 1000) + '">' +
+            '                </div>' +
+            '            </div>');
+    }
 });
